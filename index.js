4745a1a053aaea228e09a73058c3f6638da48370
Rafa Watson <rafa.watson.09@gmail.com>
2020-11-09 17:26:40 -0600

    Autorización empleando jwt

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
+//Dependencies
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
+//Routers
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
+//Middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-
-/*
-VERBOS HTTP: Formas de hacer peticiones
-GET: Obtener recursos.
-POST: Almacenar recursos o guardar.
-PATCH: Modificar una parte de un recurso.
-PUT: Modificar un recurso.
-DELETE: Borrar un recurso.
-*/
-
-app.get("/", (req, res, next) => {
-     return res.status(200).json({ code: 1, message: "Bienvenido al Pókedex"});
-});
-
-app.use("/pokemon", pokemon);
+app.get("/", index);
 app.use("/user", user);
-
-app.use((req, res, next) => {
-    return res.status(404).json({code:404, message: "URL no encontrada"});
-});
+app.use(auth);
+app.use("/pokemon", pokemon);
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
