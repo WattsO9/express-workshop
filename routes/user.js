c25042e106a309842d1c0a87cf12d51e76256cc4
Rafa Watson <rafa.watson.09@gmail.com>
2020-11-18 16:35:39 -0600

    Conectando la interfaz de signin con el servidor

--- routes/user.js
+++ routes/user.js
@@ -37,7 +37,7 @@ user.post("/login", async (req, res, next) =>{
             return res.status(200).json({code: 401, message: "Usuario y/o contraseña incorrectos"});
         }
     }
-    return res.status(500).json({code: 500, message: "Campos incompletos"});
+    return res.status(200).json({code: 500, message: "Campos incompletos"});
 
 });
 
